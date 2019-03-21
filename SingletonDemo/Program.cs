ceac4bc176de18e16b50d9b5ec81ef2f0e0315fe
kavitha <kavitha.parupati@umassmed.edu>
2019-03-21 10:03:08 -0400

    Initial commit

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PrintFirstMethod();
+
+            PrintSecondMethod();
+
+            Console.WriteLine("--------------------------------------");
+            /* This method is avoiding the singleton pattern because count value is showing 2 that means its instantiating 2 times.
+             to avoid this scenario we need to add sealed class in the singleton method*/
+            //Singleton.DerivedSingleton derviedObj = new Singleton.DerivedSingleton();
+            //derviedObj.PrintDetails("Hello Inheritance");
+            Console.ReadLine();
+        }
+
+        private static void PrintSecondMethod()
+        {
+            var s2 = Singleton.GetInstance;
+            s2.PrintDetails("Still count value is 1 means its instantiantiating only once and 2nd message");
+        }
+
+        private static void PrintFirstMethod()
+        {
+            var s1 = Singleton.GetInstance;
+            s1.PrintDetails("This is fist message");
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    /*
+     *sealed restricts the inheritance
+     */
+    public sealed class Singleton
+    {
+        private static int counter;
+
+        private static Singleton instance;
+        /*
+         * Private constructor ensures that object is not
+         * instantiated other than with in the class itself
+         */
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter);
+        }
+        /*
+         * Public property is used to return only one instance of the class
+         * levearging on the private property
+         */
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        //cannot inherit the singleton method
+        //public class DerivedSingleton : Singleton
+        //{
+
+        //}
+    }
+}
